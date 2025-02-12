62ef1f84dd31126ae03735718e15059d562df446
Robin <robin@russellpuff.net>
2025-02-12 15:22:55 -0500

    implement suspnd

--- kernel/system/suspend.c
+++ kernel/system/suspend.c
@@ -9,7 +9,12 @@
  *  thread's  state  to  suspended  and  raises a  RESCHED  syscall to schedule a  *
  *  different thread.  Returns the threadid to confirm suspension.                 */
 int32 suspend_thread(uint32 threadid) {
-
-  
+  if(threadid < 0 || threadid >= NTHREADS || 
+    (thread_table[threadid].state != TH_RUNNING && 
+      thread_table[threadid].state != TH_READY)) {
+    return -1;
+  }
+  thread_table[threadid].state = TH_SUSPEND;
+  raise_syscall(RESCHED);
   return 0;
 }
