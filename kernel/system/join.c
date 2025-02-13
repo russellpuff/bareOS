2783b6e07dcd215cdbbe83a161496c13148aa58b
Robin <robin@russellpuff.net>
2025-02-13 14:44:05 -0500

    implement join

--- kernel/system/join.c
+++ kernel/system/join.c
@@ -8,6 +8,14 @@
  *  mark  the thread  as TH_FREE  and return its  `retval`.   Otherwise  *
  *  raise RESCHED and loop to check again later.                         */
 int32 join_thread(uint32 threadid) {
+  /* TODO: Check if there's invalid states for this. */
+  if(threadid < 0 || threadid >= NTHREADS || thread_table[threadid].state == TH_FREE) {
+    return -1;
+  }
 
-  return 0;
+  while(thread_table[threadid].state != TH_DEFUNCT) {
+    raise_syscall(RESCHED);
+  }
+  thread_table[threadid].state = TH_FREE;
+  return thread_table[threadid].retval;
 }
