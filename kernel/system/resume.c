c62813f5b13d544a9f350176a45e2da625565d93
Robin <robin@russellpuff.net>
2025-02-16 20:09:46 -0500

    fix resume

--- kernel/system/resume.c
+++ kernel/system/resume.c
@@ -1,15 +1,15 @@
 #include <barelib.h>
 #include <interrupts.h>
 #include <syscall.h>
+#include <thread.h>
 
 /*  Takes a index into the thread table of a thread to resume.  If the thread is already  *
  *  ready  or running,  returns an error.  Otherwise, adds the thread to the ready list,  *
  *  sets  the thread's  state to  ready and raises a RESCHED  syscall to  schedule a new  *
  *  thread.  Returns the threadid to confirm resumption.                                  */
 int32 resume_thread(uint32 threadid) {
-  if(threadid < 0 || threadid >= NTHREADS || 
-    thread_table[threadid].state == TH_RUNNING && 
-      thread_table[threadid].state == TH_READY) {
+  if(threadid >= NTHREADS || 
+      thread_table[threadid].state != TH_SUSPEND) {
     return -1;
   }
   thread_table[threadid].state = TH_READY;
