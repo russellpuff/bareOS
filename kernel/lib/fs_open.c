bf4b6ad0008b8fc8515341f6381e747605f7ac72
Robin <robin@russellpuff.net>
2025-05-03 22:14:00 -0400

    do open

--- kernel/lib/fs_open.c
+++ kernel/lib/fs_open.c
@@ -1,5 +1,15 @@
 #include <barelib.h>
 #include <fs.h>
+#include <string.h>
+
+/*
+int16 strcmp2(const char* str1, const char* str2) {
+  for(; *str1 == *str2; str1++, str2++) {
+    if(*str1 == '\0') { return 0; }
+  }
+  return (unsigned char)*str1 - (unsigned char)*str2;
+}
+*/
 
 extern fsystem_t* fsd;
 extern filetable_t oft[NUM_FD];
@@ -10,5 +20,42 @@ extern filetable_t oft[NUM_FD];
  *  inode in the root directory.                                        *
  *  'head' is initialized to the start of the file.                     */
 int32 open(char* filename) {
-  return 0;
+	/* Try to find the file by name. */
+	int16 slot = -1;
+	for(int16 i = 0; i < DIR_SIZE; ++i) {
+		if(!fsd->root_dir.entry[i].name[0])  {
+			continue;
+		}
+		if(!strcmp(filename, fsd->root_dir.entry[i].name)) {
+			slot = i;
+			break;
+		}
+	}
+	if(slot == -1) return -1;
+
+	/* Check if it's open. */
+	for (int16 i = 0; i < NUM_FD; ++i)
+		if (oft[i].direntry == slot && oft[i].state == FSTATE_OPEN) return -1;
+
+	/* Look for available oft slot. */
+	int16 fd = -1;
+	for(int i = 0; i < NUM_FD; ++i) {
+		if(oft[i].state == FSTATE_CLOSED) {
+			fd = i;
+			break;
+		}
+	}
+	if(fd == -1) return -1;
+
+	/* Read inode. */
+	inode_t inode;
+	if(read_bs(fsd->root_dir.entry[slot].inode_block, 0, &inode, sizeof(inode_t)) < 0) return -1;
+
+	/* Populate oft slot with info. */
+	oft[fd].state = FSTATE_OPEN;
+	oft[fd].head = 0;
+	oft[fd].direntry = slot;
+	oft[fd].inode = inode;
+
+	return fd;
 }
