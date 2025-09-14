80fc04b5a3a23f275addbc7f2e9acdc26263b9f5
Robin <robin@russellpuff.net>
2025-09-14 02:15:38 -0400

    do new fs format

--- kernel/include/fs/format.h
+++ kernel/include/fs/format.h
@@ -3,63 +3,83 @@
 
 #include <lib/barelib.h>
 
-#define FS_MAGIC 0x62726673
+#define FS_MAGIC 0x62726673 /* 'brfs' - bare ram filesystem */
 #define FS_VERSION 2
 
-#define FILENAME_LEN 32         /* Arbitrary maximum length of a filename in the FS           */
-#define MDEV_BLOCK_SIZE 512     /* Size of each block in bytes                                */
-#define MDEV_NUM_BLOCKS 4096    /* Number of blocks in the block device                       */
+#define FILENAME_LEN 32         /* Arbitrary maximum length of a filename in the FS */
+#define MDEV_BLOCK_SIZE 512     /* Size of each block in bytes                      */
+#define MDEV_NUM_BLOCKS 4096    /* Number of blocks in the block device             */
 
-/* 'inode_t' are stored in the block device and contain all of the information needed by the             *
- * file system to read and write to/from a given file.  Each file has a single 'inode'.                  */
-typedef struct inode {
-	uint32_t id;                      /* Unique 'id' of the inode, corresponds with the block index          */
-	uint32_t size;                    /* The size of the file in bytes                                       */
-	uint32_t blocks[INODE_BLOCKS];    /* An array containing the indices of the blocks allocated to the file */
+#define MAX_INTABLE_BLOCKS 128  /* Completely arbitrary. TODO: Replace array.       */
+#define OFT_MAX 32              /* Also arbitrary.                                  */
+
+typedef enum { FREE, FILE, DIR } EN_TYPE;
+typedef enum { CLOSED, OPEN } FSTATE;
+typedef enum { RD_ONLY, WR_ONLY, RDWR, APPEND } FMODE;
+
+/* 'inode_t' are stored in the block device and contain all of the information needed by the file system      *
+ * to read from and write to a given file or directory. Eact dirent (a file or directory) has a single inode. *
+ * All inodes are serialized to the block device in a secret non-inode chain of blocks, walkable via the fsd  *
+ * using fsd->device.intable_blocks, each element in this array is the same as walking a FAT path.            */
+typedef struct {
+	uint32_t size;       /* Size of the data in bytes                                       */
+	EN_TYPE type;        /* Type of entry: file or directory | free means safe to overwrite */
+	int16_t head;        /* Index to the head block                                         */
+	int16_t parent;      /* Index of inode table entry holding this entry's parent          */
+	uint32_t modified;   /* UNUSED - Last modified in Unix time                             */
 } inode_t;
 
-
-/* The root directory contains DIR_SIZE directory entries.  This 'dirent_t' contains the metadata  *
- * necessary for mapping filenames to file inodes.                                                 */
-typedef struct dirent {
-	uint32_t inode_block;            /* The index of the block containing the inode for this file */
-	char name[FILENAME_LEN];       /* The name associated with this file in a directory         */
-} dirent_t;
-
-
-/* The root directory is represented as a 'directory_t' structure in the file system.  It contains *
- * the list of entries in the direcotory.                                                          */
+/* 'dirent_t' are directory entries (either a file or directory) that have a single inode associated with     *
+ * them. Each group of dirent_t inside a directory have their struct data serialized to the parent's inode    *
+ * blocks.                                                                                                    */ 
 typedef struct {
-	uint32_t numentries;             /* The number of entries used in the directory   */
-	dirent_t entry[DIR_SIZE];      /* An array of 'dirent_t' containing the entries */
-} directory_t;
-
+	uint16_t inode;          /* Index of the inode table entry holding this entry's inode       */
+	EN_TYPE type;            /* Type of entry: file or directory | free means safe to overwrite */
+	char name[FILENAME_LEN]; /* The entry's human-readable name                                 */
+} dirent_t;
 
 /* The file system contains a 'bdev_t' that stores information about the underlying block device   */
 typedef struct {
-	uint32_t nblocks;                /* The total number of blocks in the block device */
-	uint32_t blocksz;                /* The size of each block in bytes                */
+	uint16_t num_blocks;   /* The total number of blocks in the block device. May not equal the constant  */
+	uint16_t block_size;   /* The size of each block in bytes. May not equal the constant                 */
+	byte* ramdisk;         /* A pointer to the head of the ramdisk block region                           */
 } bdev_t;
 
-
-/* The 'fsystem_t' is the master record that directly or indirectly contains all of the information *
- * about the file system.  In bareOS, there is one 'fsystem_t' instance called 'fsd'                *
- *    (see system/fs.c)                                                                             */
+/* The 'filetable_t' struct is used to store information about a currently open file. An array  *
+ * of these makes up the oft in the fsd that represents all open files. Each entry is           *
+ * associated with a file when it is opened and deassociated with it is closed.                 */
 typedef struct {
-	bdev_t device;                 /* The 'bdev_t' the describes the FS's block device                  */
-	uint32_t freemasksz;             /* The size of the bitmask storing the free/used bits for each block */
-	byte* freemask;                /* A pointer to the free bitmask, each bit corresponds to a block    */
-	directory_t root_dir;          /* The 'directory_t' that stores the root directory information      */
-} fsystem_t;
-
-/* The 'filetable_t' struct is used to store information about a currently open file. It is used in   *
- * one place, an open file table ('oft') of 'filetable_t' that represent a list of open files.        *
- * Each entry is associated with a file when it is opened and removed with it is closed.              */
-typedef struct {
-	byte state;                    /* The current state of the entry, either FSTATE_OPEN or FSTATE_CLOSED */
-	uint32_t head;                   /* The byte in the file at which the next operation is performed       */
-	uint32_t direntry;               /* A reference to the directory entry where the file came from (index) */
-	inode_t inode;                 /* A copy of the inode of the file (read from the block device)        */
+	FSTATE state;        /* The current state of the entry, either FSTATE OPEN or CLOSED        */
+	FMODE mode;          /* The current ops mode. Read, write, read/write, or append            */
+	uint16_t curr_block; /* The FAT index of the current block being looked at by ops           */
+	uint16_t curr_index; /* Which MDEV_BLOCK_SIZE-sized chunk in the file curr_block represents */
+	inode_t inode;       /* In-memory copy of the inode.                                        */
+	bool in_dirty;       /* A flag saying whether the inode copy has to be written back         */
 } filetable_t;
+
+/* 'fsuper_t' represents serializable data stored in the super block, it contains key   *
+ * data required to remake the fs after a reboot. Many of its values are necessary for  *
+ * the in-RAM filesystem_t to function.                                                 */
+typedef struct {
+	uint32_t blk_count;    /* Number of blocks available for the filesystem to write to */
+	uint32_t blk_size;     /* The size of each block in bytes                           */
+	uint32_t magic;        /* Magic number used to identify the type of filesystem      */
+	byte version;          /* The filesystem's version                                  */
+	uint16_t fat_head;     /* Index of the first FAT table block                        */
+	uint16_t fat_size;     /* The number of blocks the FAT table occupies               */
+	uint16_t intable_head; /* Index of the first inode table block                      */
+	uint16_t intable_size; /* Current capacity of innode table given allocated blocks   */
+	uint16_t root_inode;   /* Index in the inode table of the root directory inode      */
+	uint16_t intable_blocks[MAX_INTABLE_BLOCKS]; /* FAT path of inode table blocks for O(1) lookup time */
+	byte intable_numblks;  /* Number of blocks ACTUALLY used by the inode table         */
+} fsuper_t;
+
+/* 'fsystem_t' is the combined overarching master record of all the information about the *
+ * ramdisk fs. In bareOS, there is only one 'fsystem_t' instance in memory called fsd.    */
+typedef struct {
+	bdev_t device;
+	fsuper_t super;
+	filetable_t oft[OFT_MAX];
+} fsystem_t;
 
 #endif
--- kernel/include/fs/fs.h
+++ kernel/include/fs/fs.h
@@ -4,43 +4,37 @@
 #include <lib/barelib.h>
 #include <fs/format.h>
 
-#define EMPTY -1                /* Used in FS whenever a field's state is undefined or unused */
+#define EMPTY -1        /* Used in FS whenever a field's state is undefined or unused */
 
-#define SB_BIT 0                /* Alias for the super block index                            */
-#define BM_BIT 1                /* Alias for the bitmask block index                          */
+#define SB_BIT 0        /* Alias for the super block index                            */
+#define BM_BIT 1        /* Alias for the bitmask block index                          */
 
-#define FSTATE_CLOSED 0         /* Used when opening and closing files to indicate the state  */
-#define FSTATE_OPEN   1         /*     of the slot in the open file table.                    */
-#define NUM_FD       10         /* Number of slots in the open file table                     */
-
-#define SEEK_START 0            /* Used in `fs_seek`, count from start of file                */
-#define SEEK_END   1            /* Used in `fs_seek`, count down from the end of the file     */
-#define SEEK_HEAD  2            /* Used in `fs_seek`, move head relative to the current head  */
+#define SEEK_START 0    /* Used in `seek`, count from start of file                   */
+#define SEEK_END   1    /* Used in `seek`, count down from the end of the file        */
+#define SEEK_HEAD  2    /* Used in `seek`, move head relative to the current head     */
 
 /* Function prototypes used in the file system */
-bdev_t bdev_getstats(void);                       /* Retreive the embedded block dev info        */
-uint32_t mk_ramdisk(uint32_t, uint32_t);              /* Build the block device                      */
-uint32_t free_ramdisk(void);                      /* Free resources associated with block device */
+bdev_t bdev_getstats(void);                               /* Retreive the embedded block dev info        */
+uint32_t mk_ramdisk(uint32_t, uint32_t);                  /* Build the block device                      */
+uint32_t free_ramdisk(void);                              /* Free resources associated with block device */
 uint32_t read_bdev(uint32_t, uint32_t, void*, uint32_t);  /* Read a block from the block device          */
 uint32_t write_bdev(uint32_t, uint32_t, void*, uint32_t); /* Write a block to the block device           */
 
-void   setmaskbit_fs(uint32_t);                   /* Mark a block as used                        */
-void   clearmaskbit_fs(uint32_t);                 /* Mark a block as unused                      */
-uint32_t getmaskbit_fs(uint32_t);                   /* Get the state of a block                    */
+void   setmaskbit_fs(uint32_t);   /* Mark a block as used      */
+void   clearmaskbit_fs(uint32_t); /* Mark a block as unused    */
+uint32_t getmaskbit_fs(uint32_t); /* Get the state of a block  */
 
-void mkfs(void);                                /* Save the super block and bitmask for the FS */
-uint32_t mount_fs(void);                          /* Build the structures for the file system    */
-uint32_t umount_fs(void);                         /* Clear the structures for the file system    */
+void mkfs(void);          /* Save the super block and bitmask for the FS */
+uint32_t mount_fs(void);  /* Build the structures for the file system    */
+uint32_t umount_fs(void); /* Clear the structures for the file system    */
 
-int32_t create(char*);                            /* Create a file and save it to the block device */
-int32_t open(char*);                              /* Open a file                                   */
-int32_t close(int32_t);                             /* Close a file                                  */
-int32_t write(uint32_t, char*, uint32_t);               /* Write to a file                               */
-int32_t read(uint32_t,char*,uint32_t);                /* Read from file                                */
-uint32_t get_filesize(uint32_t);
+int32_t create(char*);                    /* Create a file and save it to the block device */
+int32_t open(char*);                      /* Open a file                                   */
+int32_t close(int32_t);                   /* Close a file                                  */
+int32_t write(uint32_t, char*, uint32_t); /* Write to a file                               */
+int32_t read(uint32_t,char*,uint32_t);    /* Read from file                                */
+uint32_t get_filesize(uint32_t);          /* Quick size lookup                             */
 
 extern fsystem_t* fsd;
-extern filetable_t oft[NUM_FD];
-extern uint16_t fat_table[MDEV_NUM_BLOCKS];
 
 #endif
